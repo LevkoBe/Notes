0d4cff42b7ce08d138fc7cfa5d86746ee093f1c8
Levko Beniakh <lbeniakh@kse.org.ua>
2024-03-01 14:33:42 +0200

    Second attempt to restructure

---
+++ NotesMicroservice/notesmicroservice/notemicro/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NotemicroConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notemicro'
--- handlers/userCookie.js
+++
@@ -1,8 +0,0 @@
-const setTokenCookie = (res, userId) => {
-    res.cookie('token', userId, {
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    });
-};
-
-module.exports = setTokenCookie;
